49c0ec04be633313706d79497be4b45952b2bb5a
MudiaUON <mudiauon@gmail.com>
2023-01-16 20:57:15 +0000

    fixed navbar responsiveness

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+// Get the hamburger menu element
+var menu = document.querySelector(".hamburger-menu");
 
-  
-
+//navbar animation
 function changeIcon(icon) {
-    icon.classList.toggle("change");
-    
-  }
+  icon.classList.toggle("change");
+  menu.classList.toggle("show");
+}
